78772125e953d4b6a1442ea630af9d305d734d1b
Gabriel Simioni <gabrielsimioniwo@gmail.com>
2025-12-08 22:24:15 -0300

    iniciando React

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,14 @@
+import "./banner.css";
+
+
+function Banner() {
+    return (
+        <header className="banner">
+             <img src="/images/banner.png" alt="Banner Principal da Fennec" />
+        </header>
+       
+    );
+        
+}
+
+export default Banner;
