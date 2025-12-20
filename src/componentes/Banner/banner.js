f22e3bcbeec977b09f24ffa7587ac0da6bcdbb88
Gabriel Simioni <gabrielsimioniwo@gmail.com>
2025-12-20 16:38:28 -0300

    dbfgnd

--- src/componentes/Banner/banner.js
+++ src/componentes/Banner/banner.js
@@ -1,14 +1,22 @@
 import "./banner.css";
 
-
 function Banner() {
-    return (
-        <header className="banner">
-             <img src="/images/Fennec_banner" alt="Banner Principal da Fennec" />
-        </header>
-       
-    );
-        
+  return (
+    <header className="hero">
+      <div className="hero-overlay"></div>
+
+      <div className="hero-content">
+        <h1>FENNEC</h1>
+        <p>Consultoria Empresarial</p>
+
+        <div className="hero-metrics">
+          <span>36+ Clientes</span>
+          <span>10+ Anos</span>
+          <span>95% Satisfação</span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
